e5555c0cd9a32e91eec595f0d42bc96424101bc4
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2023-02-12 11:51:41 -0800

    added form syntax to edit

--- mern-bucket-list/src/components/edit.jsx
+++ mern-bucket-list/src/components/edit.jsx
@@ -7,6 +7,38 @@ export default function Edit() {
              <h1>Edit Bucket Page</h1>
              </main>
         <Navbar></Navbar>
+        <Form>
+                <Form.Group className="mb-3" controlId="activity">
+                    <Form.Label>Bucket List Activity</Form.Label>
+                    <Form.Control type="activity" placeholder="Activity" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="description">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control type="description" placeholder="Description" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="Checkbox1">
+                    <Form.Label>1 skull</Form.Label>
+                    <Form.Check type="checkbox1" label="Checkbox 1" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="Checkbox2">
+                    <Form.Label>2 skulls</Form.Label>
+                    <Form.Check type="checkbox2" label="Checkbox 2" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="Checkbox3">
+                    <Form.Label>3 skulls</Form.Label>
+                    <Form.Check type="checkbox3" label="Checkbox 3" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="author">
+                    <Form.Label>Author</Form.Label>
+                    <Form.Control type="author" placeholder="Author" />
+                </Form.Group>
+                <Button variant="primary" type="cancel">
+                    Cancel
+                </Button>
+                <Button variant="info" type="edit">
+                    Edit
+                </Button>
+            </Form>
     </div>
     )
 }
