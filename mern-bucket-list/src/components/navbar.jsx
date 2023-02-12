af7ca13ed40cb74886e1586ab93e238df01dcae0
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2023-02-11 17:34:23 -0800

    added images to navbar.jsx

--- mern-bucket-list/src/components/navbar.jsx
+++ mern-bucket-list/src/components/navbar.jsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom'
 import '../styles.css'
+import heybucket from '../assets/heybucket.png'
+import dontkickbucket from '../assets/dontkickbucket.png'
 
 export default function Navbar() {
     return(
         
         <nav className='nav' >
-            <img src='../assets/heybucket.png' alt='Hey, Whats your Bucklist?'></img>
+            <img src={heybucket} alt="Hey bucket logo" height={150} width={150}/>
             <div className='buttons'>
 
                  {/* temporary link */}
+
                 <Link to='/'>
                     <br />
                     <button type="button" class="btn btn-warning">Intro</button>
@@ -16,27 +19,27 @@ export default function Navbar() {
 
                 <Link to='/Home'>
                     <br></br>
-                    <button type="button" class="btn btn-warning">Home</button>
+                    <button type="button" class="btn btn-primary">Home</button>
                 </Link>
                 
                 <Link to='/Create'>
                     <br />
-                    <button type="button" class="btn btn-warning">Create</button>
+                    <button type="button" class="btn btn-success">Create</button>
                 </Link>
 
                  {/* temporary link */}
                 <Link to='/Edit'>   
                     <br></br>
-                    <button type="button" class="btn btn-warning">Edit</button>
+                    <button type="button" class="btn btn-info">Edit</button>
                 </Link>
 
                  {/* temporary link */}
                 <Link to='/Delete'>
                     <br />
-                    <button type="button" class="btn btn-warning">Delete</button>
+                    <button type="button" class="btn btn-danger">Delete</button>
                 </Link>
             </div>
-            <img src='./assets/dontkckbucket.png' alt='Dont Kick Bucket'></img>
+            <img src={dontkickbucket} alt="Dont kick bucket" height={150} width={150}/>
         </nav>
         
     )
