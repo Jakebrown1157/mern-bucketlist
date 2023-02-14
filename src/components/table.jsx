4a62074854909a2b4cd75c53d523f00a662fcccb
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2023-02-13 21:10:34 -0800

    adjusted background color to table

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -3,7 +3,7 @@ import '../styles.css'
 
 function Table() {
     return(
-            <div style={{backgroundColor: '#EDFF59', width: '80vw', margin: 'auto'}}>
+            <div style={{backgroundColor: '#3B5664', width: '80vw', margin: 'auto'}}>
                 <p>test buckets</p>
                 <p>test buckets</p>
                 <p>test buckets</p>
