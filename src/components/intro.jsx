487974a95f4c7cafcc9f17f9da91e298f0038773
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2023-02-18 12:55:07 -0800

    changed colors on styling

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -10,7 +10,7 @@ function Intro() {
                 <img className="img_i" src={heybucket} alt="Hey bucket logo" />
                
             </nav>
-
+            <br></br>
             <div className='intro'>
                 <p className='intro_p'>
                 <p>My partner asked me if I'd ever seen "The Bucket List."
@@ -22,7 +22,7 @@ function Intro() {
                 </p>
             </div>
             <br></br>
-            <Link to='/home' style={{ display: 'flex', padding: 20, justifyContent: 'center', width: '80vw', margin: 'auto'}}>
+            <Link to='/home' style={{ display: 'flex', padding: 20, justifyContent: 'center', width: '80vw', margin: 'auto' , textDecoration: "none"}}>
                 <br></br>
                 <button type="button" className="btn btn-danger">ENTER IF YOU DARE!!</button>
             </Link>
