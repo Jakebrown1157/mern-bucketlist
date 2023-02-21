c3535d9f4ce3dcd4ea4aea7bc866b3ea71745316
jake <jakebrown536@gmail.com>
2023-02-21 01:23:41 -0700

    ready for asure

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -23,8 +23,8 @@ function Home() {
 
     return (
 
-      <div>
-        <div>
+      <div style={{ height: '900px'}}>
+        <div style={{ display: 'flex', padding: 20, justifyContent: 'center', color: 'white' }}>
             <h1> Hey, Whats Your Bucket List?</h1>
         </div>
         <Navbar></Navbar>
@@ -32,22 +32,21 @@ function Home() {
         <main>
           {buckets && (
             <div>
-              <Table bordered hover id="formStyle">
+              <Table striped bordered hover variant="dark" style={{maxWidth: "90%", marginLeft: "5%"}}>
                   <thead>
                     <tr>
-                      <th id='homeTableHeaderColor'>Bucket List Item</th>
-                      <th id='homeTableHeaderColor'>Difficulty</th>
-                      <th id='homeTableHeaderColor'>Author</th>
-                      <th id='homeTableHeaderColor'>Description</th>
+                      <th>Bucket List Item</th>
+                      <th>Difficulty</th>
+                      <th>Author</th>
+                      <th>Description</th>
                     </tr>
                   </thead>
               {buckets.map((bucket, index) => {
                 return(
                   
-
                   <tbody key={index}>
                     <tr key={index}> 
-                        <td ><Link id='homeTableBodyColor' style={{textDecoration: "none", color: 'white'}} to={`/Edit/${bucket.id}`}> {bucket.name}</Link></td>
+                        <td ><Link style={{textDecoration: "none", color: 'white'}} to={`/Edit/${bucket.id}`}> {bucket.name}</Link></td>
                         <td>{bucket.difficulty} </td>
                         <td>{bucket.author} </td>
                         <td>{bucket.description}</td>
