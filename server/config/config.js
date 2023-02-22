6f27261272f8dc06c3cab3ea36c0c6aff7cf0ccb
Alexis Delgado <alexisdelgado@Alexis-M1MBPro.localdomain>
2023-02-22 15:43:53 -0800

    added config file back to system, deleted config from gitignore

---
+++ server/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports ={
+  development: {
+    username: 'postgres',
+    password: 'Y4na8EstEs$',
+    database: 'postgres',
+    host: 'db.vddrpzmpfqixaygmlpzg.supabase.co',
+    dialect: "postgres"
+  },
+  test: {
+    username: 'postgres',
+    password: 'Y4na8EstEs$',
+    database: 'postgres',
+    host: 'db.vddrpzmpfqixaygmlpzg.supabase.co',
+    dialect: "postgres"
+  },
+  production: {
+    username: 'postgres',
+    password: 'Y4na8EstEs$',
+    database: 'postgres',
+    host: 'db.vddrpzmpfqixaygmlpzg.supabase.co',
+    dialect: "postgres"
+  }
+}
